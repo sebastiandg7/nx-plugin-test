37e4133240323913417293a026fc43735fc687d8
Sebastiandg7 <sebastiandg7@gmail.com>
2021-02-17 10:33:19 -0500

    feat(build): add greeting message

--- packages/nx-plugin-test/src/executors/build/executor.ts
+++ packages/nx-plugin-test/src/executors/build/executor.ts
@@ -1,7 +1,12 @@
+import { logger } from '@nrwl/devkit';
 import { BuildExecutorSchema } from './schema';
+import * as chalk from 'chalk';
+
+const PLUGIN_PREFIX = `${chalk.bold(chalk.yellow(`> ${chalk.inverse(' PLUGIN-TEST ')}`))}`;
 
 export default async function runExecutor(options: BuildExecutorSchema) {
   console.log('Executor ran for Build', options);
+  logger.info(`${PLUGIN_PREFIX} ${chalk.bold('Holi :)!')}`);
   return {
     success: true,
   };
