9efcba9f79869c84d6ef4100ab34d664ee13cbd8
Sebastiandg7 <sebastiandg7@gmail.com>
2021-02-18 18:31:08 -0500

    feat(plugin): print in magenta

--- packages/nx-plugin-test/src/executors/build/executor.ts
+++ packages/nx-plugin-test/src/executors/build/executor.ts
@@ -1,7 +1,14 @@
 import { BuildExecutorSchema } from './schema';
+import * as chalk from 'chalk';
 
 export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build! A little change, another one', options);
+  console.log(
+    `${chalk.inverse(
+      chalk.magenta(
+        'Executor ran for Build! A little change, another one, options'
+      )
+    )}`
+  );
   return {
     success: true,
   };
