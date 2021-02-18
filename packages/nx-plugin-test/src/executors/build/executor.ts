2f9f0a3fca2e9604ca317cb661fd0d699abb14cb
Sebastiandg7 <sebastiandg7@gmail.com>
2021-02-18 15:10:15 -0500

    feat(build): extra logs!

--- packages/nx-plugin-test/src/executors/build/executor.ts
+++ packages/nx-plugin-test/src/executors/build/executor.ts
@@ -1,7 +1,7 @@
 import { BuildExecutorSchema } from './schema';
 
 export default async function runExecutor(options: BuildExecutorSchema) {
-  console.log('Executor ran for Build! A little change', options);
+  console.log('Executor ran for Build! A little change, another one', options);
   return {
     success: true,
   };
